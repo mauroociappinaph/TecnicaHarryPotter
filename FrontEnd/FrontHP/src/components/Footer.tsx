14d2b74ee92c84ff0974cb848198e8293d053bda
MAURO CIAPPINA <100035249+mauroociappinaph@users.noreply.github.com>
2024-10-04 17:31:22 -0300

    footer mejorado

--- FrontEnd/FrontHP/src/components/Footer.tsx
+++ FrontEnd/FrontHP/src/components/Footer.tsx
@@ -1,7 +1,46 @@
+import { FaGithub, FaLinkedin, FaGlobe } from "react-icons/fa";
+
 const Footer = () => {
+  // Función para obtener solo el año actual
+  const getCurrentYear = () => new Date().getFullYear();
+
   return (
-    <footer className="py-10">
-      <span className="text-indigo-600">Harry Potter &copy; 2024</span>
+    <footer className="w-full py-8 bg-indigo-100 text-center mt-10">
+      <div className="container mx-auto px-4 flex flex-col items-center space-y-4 md:flex-row md:justify-between">
+        <span className="text-indigo-600 font-bold">
+          Harry Potter &copy; {getCurrentYear()}
+        </span>
+
+        <div className="flex space-x-4">
+          <a
+            href="https://github.com/mauroociappinaph"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="GitHub"
+            className="text-indigo-700 hover:text-indigo-500"
+          >
+            <FaGithub size={24} />
+          </a>
+          <a
+            href="https://maurociappina.vercel.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Portafolio Web"
+            className="text-indigo-700 hover:text-indigo-500"
+          >
+            <FaGlobe size={24} />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/maurojoseciappina/"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="LinkedIn"
+            className="text-indigo-700 hover:text-indigo-500"
+          >
+            <FaLinkedin size={24} />
+          </a>
+        </div>
+      </div>
     </footer>
   );
 };
